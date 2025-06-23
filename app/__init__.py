62f9b5a73eb2368d9a256b324168a8421e974bb1
Chromiumore <parts.dima06@gmail.com>
2025-06-23 15:49:03 +0300

    main page example

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import app.recepies
+import app.recipes
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_pyfile('config.py', silent=False)
 
-    app.register_blueprint(recepies.bp)
-    app.route('/')(recepies.default)
+    app.register_blueprint(recipes.bp)
+    app.route('/')(recipes.default)
 
     return app
