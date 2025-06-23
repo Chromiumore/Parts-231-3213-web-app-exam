4c88f3a929c51313332d4f713f5d401bbd2b5276
Chromiumore <parts.dima06@gmail.com>
2025-06-23 18:53:19 +0300

    recipe form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import app.recipes
+import app.recipes as recipes
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
--- app/recipes.py
+++ app/recipes.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template, redirect, url_for
 
-bp = Blueprint('recepies', __name__, url_prefix='/recepies')
+bp = Blueprint('recipes', __name__, url_prefix='/recipes')
 
 
 def default():
-    return redirect(url_for('recepies.index'))
+    return redirect(url_for('recipes.index'))
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/new')
+def new():
+    return render_template('new-recipe.html')
